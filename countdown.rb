c79aad6448c8f9515cad4255bb25cae2639d3774
Karl Benbeneck <kbenbeneck@gmail.com>
2018-10-23 02:53:53 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 def countdown(x)
   while x > 0
     puts "#{x} SECOND(S)!"
-    x -= 1
+  x -= 1
   end
   "HAPPY NEW YEAR!"
 end
@@ -11,8 +11,8 @@ end
 def countdown_with_sleep
   while x > 0
     puts "#{x} SECOND(S)!"
-    sleep(1)
-    x -= 1
+  sleep(1)
+  x -= 1
   end
   "HAPPY NEW YEAR!"
 end
